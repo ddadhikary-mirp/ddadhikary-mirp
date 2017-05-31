946f4caa12c37637097c9a187495d1cb631f9a0a
ddadhikary-mirp <ddadhikary-mirp@mit.edu>
2017-06-01 01:14:51 +0530

    adding god

---
+++ my_class/bag.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main () {
+int a,b,c,d;
+cout << "Enter the cut off marks A:" ;
+cin >> a;
+cout << "Enter the cut off marks B:" ;
+cin >> b;
+cout << "Enter the cut off marks C:" ;
+cin >> c;
+cout << "Enter the obtained number:" ;
+cin >> d;
+if (a>=100) {
+  if (b>=60){
+    if (c>=50){
+      if(d<a){
+	if(d<b){
+	  if(d>=c){
+	    if(d>=c){
+}		cout << "Your grade is C" ;
+else 
+}	cout << "You have failed" ;
+else
+}	cout << "Your grade is B" ;
+else
+}	cout << "Your grade is A" ;
+else 
+}	cout << "Invalid cut off marks" ;
+else
+}	cout << "Invalid cut of marks" ;
+else
+	cout << "Invalid cut off marks" ;
+
+	return 0 ;
+}
---
+++ my_class/god.cpp~
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main () {
+int n,average;
+cout << "Enter a number:" ;
+cin >> n;
+average = n*(n+1)/(2*n) ;
+cout << "The average of" << n << "numbers is" << average << endl;
+return 0;
+}
