bb9f14b7c2e3ac254b9bf84a70edd36362f95f6b
ddadhikary-mirp <ddadhikary-mirp@mit.edu>
2017-06-05 00:57:02 +0530

    adding me

--- my_pen/gun.cpp
+++ my_pen/gun.cpp
@@ -1,5 +1,18 @@
-#include <iostream.h>
+
+#include <iostream>
+using namespace std;
 int main () {
 int i,j,n;
 cout << "Enter numbers of rows:" ;
-
+cin >> n;
+cout << "The pattern is : \n" ;
+for (i=1; i<=n;i++)
+{
+for (j=1;j<=n;j++)
+{
+cout << j*i;
+}
+cout << " " << endl;
+}
+return 0;
+}
